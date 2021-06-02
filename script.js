550b8149a8fb8411835e3d7efe0fb7472f8d2eac
Tejas Phirke <tejasphirke.steja@gmail.com>
2021-06-02 21:50:24 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    webkitURL: 'https://lottiefiles.com/64003-blue'
+})
